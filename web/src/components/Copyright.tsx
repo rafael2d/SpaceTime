93a140b7b45e03af0e2ea5bff54e1ad2a0fe3ff4
Rafael D'Angelis Diniz Nogueira <rr.dangelis@gmail.com>
2023-06-05 16:28:07 -0300

    Versão final
    Upload do projeto concluido

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com ❤️ por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://www.instagram.com/r.v.ff/?hl=pt"
+      >
+        Rafael Diniz
+      </a>
+    </div>
+  )
+}
---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { ChangeEvent, useState } from 'react'
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewUrl = URL.createObjectURL(files[0])
+
+    setPreview(previewUrl)
+  }
+
+  return (
+    <>
+      <input
+        name="coverURL"
+        onChange={onFileSelected}
+        type="file"
+        accept="image/*"
+        className="invisible h-0 w-0"
+        id="media"
+      />
+
+      {preview && (
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </>
+  )
+}
